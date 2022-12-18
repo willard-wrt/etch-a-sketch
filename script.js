00d48dba17f1e8092996e13c6c2025708b929793
willard-wrt <119113615+willard-wrt@users.noreply.github.com>
2022-12-19 04:52:52 +0700

    Implement a grid adjustment slider and a drawing mode toggle.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //declre global variable
 let gridScreen = document.querySelector('.grid-container');
+let slideText = document.querySelector('.square-value');
+let slideValue = document.getElementById('slider');
+let drawingToggle = document.getElementById('s1');
+
 
 function createGrid() {
   for (let i = 0; i< 256; i+= 1) {
@@ -10,4 +14,68 @@ function createGrid() {
   }
 }
 
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 createGrid();
+
+drawingToggle.addEventListener('change', function() {
+    removeAllChildNodes(gridScreen);    
+    value = slideValue.value;
+    slideText.innerHTML = `${value}*${value}`;
+    gridScreen.setAttribute('style', `grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 2fr);`);
+    console.log(drawingToggle.checked);
+    if (drawingToggle.checked == true )
+    {
+    for (let i = 0; i< value*value; i+= 1) {
+        let createDiv = document.createElement('div');
+        createDiv.classList.add('cell');
+        createDiv.addEventListener('mousedown',function(e){e.target.style.backgroundColor = 'black';})
+        gridScreen.appendChild(createDiv);
+      }
+    }
+    if (drawingToggle.checked == false )
+    {
+    for (let i = 0; i< value*value; i+= 1) {
+        let createDiv = document.createElement('div');
+        createDiv.classList.add('cell');
+        createDiv.addEventListener('mouseover',function(e){e.target.style.backgroundColor = 'black';})
+        gridScreen.appendChild(createDiv);
+      }
+    }
+    
+
+});
+
+
+slideValue.addEventListener('input', function() {
+value = slideValue.value;
+slideText.innerHTML = `${value}*${value}`;
+removeAllChildNodes(gridScreen);
+gridScreen.setAttribute('style', `grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 2fr);`);
+console.log(drawingToggle.checked);
+if (drawingToggle.checked == true )
+{
+for (let i = 0; i< value*value; i+= 1) {
+    let createDiv = document.createElement('div');
+    createDiv.classList.add('cell');
+    createDiv.addEventListener('mousedown',function(e){e.target.style.backgroundColor = 'black';})
+    gridScreen.appendChild(createDiv);
+  }
+}
+if (drawingToggle.checked == false )
+{
+for (let i = 0; i< value*value; i+= 1) {
+    let createDiv = document.createElement('div');
+    createDiv.classList.add('cell');
+    createDiv.addEventListener('mouseover',function(e){e.target.style.backgroundColor = 'black';})
+    gridScreen.appendChild(createDiv);
+  }
+}
+
+})
+;
+
